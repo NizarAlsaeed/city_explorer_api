9ed5f026c619f2cf733868f2d84ae51862f52d29
Nizar Alsaeed <nizar.ayman@gmail.com>
2021-03-28 17:59:23 +0300

    feature 1 location

--- server.js
+++ server.js
@@ -8,27 +8,49 @@ const app = express();
 app.listen(PORT, ()=> console.log(`App is running on port: ${PORT}`));
 app.use(cors());
 
-app.get('/location', handleLocation);
-//app.get('/weather', weatherHandler);
+app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
+
+function LocationResposeData (search_query,formatted_query,latitude,longitude){
+    this.search_query= search_query;
+    this.formatted_query= formatted_query;
+    this.latitude= latitude;
+    this.longitude= longitude;
+}
+function WeatherResposeData (forecast,time){
+    this.forecast= forecast;
+    this.time= time;
+}
 
 
-function handleLocation(request,response){
+
+function locationHandler(request,response){
     console.log('request.query', request.query);
     const locationData = require('./data/location.json');
     console.log('locationData', locationData);
     const city = request.query.city;
     console.log('city', city);
-    let resObj={
-        search_query: city,
-        formatted_query: locationData[0].display_name,
-        latitude: locationData[0].lat,
-        longitude: locationData[0].lon,
-    };
+    const resObj= new LocationResposeData(city,locationData[0].display_name,locationData[0].lat,locationData[0].lon);
+
     console.log('resObj', resObj);
     response.send(resObj);
 
 }
 
+function weatherHandler(request,response){
+    console.log('request.query', request.query);
+    const weatherData = require('./data/weather.json');
+    console.log('weatherData', weatherData);
+    const city = request.query.city;
+    console.log('city', city);
+    let resArr=[];
+    weatherData.data.forEach(obj=> {
+        resArr.push( new WeatherResposeData(obj.weather.description,obj.datetime));
+    });
+    console.log('resArr', resArr);
+    response.send(resArr);
+
+}
 
 
 
